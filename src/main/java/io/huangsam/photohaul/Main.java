2ba4675943276dff917afb71583a2721e9c62077
Samuel Huang <samhuang91@gmail.com>
2024-11-24 19:15:37 -0800

    Collect metadata for each photo

--- src/main/java/io/huangsam/photohaul/Main.java
+++ src/main/java/io/huangsam/photohaul/Main.java
@@ -1,10 +1,16 @@
 package io.huangsam.photohaul;
 
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
 import org.slf4j.Logger;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -25,9 +31,26 @@ public class Main {
                     .filter(Files::isRegularFile)
                     .filter(Main::isPhoto)
                     .sorted(Comparator.comparingLong(Main::getLastModified))
-                    .forEach(filePath -> LOG.info(filePath.toString()));
+                    .forEach(Main::visitPhoto);
         } catch (IOException e) {
             LOG.error(e.getMessage());
+        }
+    }
+
+    private static void visitPhoto(Path path) {
+        LOG.info(path.toString());
+        try (InputStream input = Files.newInputStream(path)) {
+            Metadata metadata = ImageMetadataReader.readMetadata(input);
+            for (Directory directory : metadata.getDirectories()) {
+                for (Tag tag : directory.getTags()) {
+                    LOG.debug("dir={} name={} description={}",
+                            tag.getDirectoryName(),
+                            tag.getTagName(),
+                            tag.getDescription());
+                }
+            }
+        } catch (IOException | ImageProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -36,8 +59,8 @@ public class Main {
             return Files.probeContentType(path).startsWith("image/");
         } catch (IOException | NullPointerException e) {
             LOG.trace(e.getMessage());
-            String pathName = path.toString();
-            return Stream.of("jpg", "png", "svg").anyMatch(pathName::endsWith);
+            String pathName = path.toString().toLowerCase();
+            return Stream.of("jpg", "jpeg", "png").anyMatch(pathName::endsWith);
         }
     }
 

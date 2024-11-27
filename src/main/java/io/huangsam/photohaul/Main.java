f528db0f653905a6a30c81c82457b506432de574
Samuel Huang <samhuang91@gmail.com>
2024-11-27 08:48:06 -0800

    Refactor variable semantics in runner

--- src/main/java/io/huangsam/photohaul/Main.java
+++ src/main/java/io/huangsam/photohaul/Main.java
@@ -21,25 +21,25 @@ public class Main {
     private static final Settings SETTINGS = new Settings();
 
     public static void main(String[] args) {
-        PhotoPathVisitor visitor = new PhotoPathVisitor();
+        PhotoPathVisitor pathVisitor = new PhotoPathVisitor();
 
-        PathRuleSet pathRules = new PathRuleSet(List.of(
+        PathRuleSet pathRuleSet = new PathRuleSet(List.of(
                 Files::isRegularFile,
                 PathRule.allowedExtensions("jpg", "jpeg", "png").or(PathRule.isImageContent()),
                 PathRule.minimumBytes(100L)));
 
-        traversePhotos(SETTINGS.getSourcePath(), visitor, pathRules);
+        traversePhotos(SETTINGS.getSourcePath(), pathVisitor, pathRuleSet);
 
         Migrator migrator = new CameraPathMigrator(
                 SETTINGS.getTargetPath(), StandardCopyOption.REPLACE_EXISTING);
 
-        migratePhotos(migrator, visitor);
+        migratePhotos(migrator, pathVisitor);
     }
 
-    private static void traversePhotos(Path source, PhotoPathVisitor visitor, PathRuleSet pathRules) {
+    private static void traversePhotos(Path source, PhotoPathVisitor pathVisitor, PathRuleSet pathRuleSet) {
         LOG.info("Start traversal of {}", source);
         try (Stream<Path> fileStream = Files.walk(source)) {
-            fileStream.parallel().filter(pathRules::matches).forEach(visitor::visitPhoto);
+            fileStream.parallel().filter(pathRuleSet::matches).forEach(pathVisitor::visitPhoto);
             LOG.info("Finish traversal of {}", source);
         } catch (IOException e) {
             LOG.error("Abort traversal of {}: {}", source, e.getMessage());

ba0a86daf64a8bb4c5279034092a5ce4d263989c
Samuel Huang <samhuang91@gmail.com>
2024-11-28 14:15:33 -0800

    Fix Checkstyle for abstract migration

--- src/main/java/io/huangsam/photohaul/migration/PathMigrator.java
+++ src/main/java/io/huangsam/photohaul/migration/PathMigrator.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 
 import static org.slf4j.LoggerFactory.getLogger;
 

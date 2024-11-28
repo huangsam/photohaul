f3a7566eba3330cf3c73778002eed3a411b4afaf
Samuel Huang <samhuang91@gmail.com>
2024-11-28 15:19:52 -0800

    Remove unused constructor

--- src/main/java/io/huangsam/photohaul/migration/FlatPathMigrator.java
+++ src/main/java/io/huangsam/photohaul/migration/FlatPathMigrator.java
@@ -4,13 +4,8 @@ import io.huangsam.photohaul.model.Photo;
 
 import java.nio.file.CopyOption;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 
 public class FlatPathMigrator extends PathMigrator {
-    public FlatPathMigrator(Path targetRoot) {
-        super(targetRoot, StandardCopyOption.REPLACE_EXISTING);
-    }
-
     public FlatPathMigrator(Path targetRoot, CopyOption copyOption) {
         super(targetRoot, copyOption);
     }

ff0456db8bc444dc0bab0e940afd0736d519ca1c
Samuel Huang <samhuang91@gmail.com>
2024-11-28 20:56:08 -0800

    Remove redundant logging at visitor layer

--- src/main/java/io/huangsam/photohaul/traversal/PhotoPathVisitor.java
+++ src/main/java/io/huangsam/photohaul/traversal/PhotoPathVisitor.java
@@ -1,17 +1,12 @@
 package io.huangsam.photohaul.traversal;
 
 import io.huangsam.photohaul.model.Photo;
-import org.slf4j.Logger;
 
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
 public class PhotoPathVisitor {
-    private static final Logger LOG = getLogger(PhotoPathVisitor.class);
-
     private final ConcurrentHashMap<Path, Photo> photoIndex = new ConcurrentHashMap<>();
 
     public Collection<Photo> getPhotos() {
@@ -19,7 +14,6 @@ public class PhotoPathVisitor {
     }
 
     public void visitPhoto(Path path) {
-        LOG.trace("Visit photo {}", path.toString());
         PhotoPathBuilder pb = new PhotoPathBuilder();
         pb.fillInfo(path);
         photoIndex.put(path, pb.build());

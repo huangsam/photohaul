edcf0788c19631ba24597b7d9c849fe38bf824c5
Samuel Huang <samhuang91@gmail.com>
2024-11-30 10:18:12 -0800

    Add JavaDoc to Migrator interface

--- src/main/java/io/huangsam/photohaul/migration/Migrator.java
+++ src/main/java/io/huangsam/photohaul/migration/Migrator.java
@@ -5,9 +5,32 @@ import io.huangsam.photohaul.model.Photo;
 import java.util.Collection;
 
 public interface Migrator {
+    /**
+     * Migrate a collection photos to a specific target.
+     *
+     * <p> For each photo record, do the following:
+     *
+     * <ul>
+     *     <li>Use its fields to determine where to place contents</li>
+     *     <li>Increment successful operations</li>
+     *     <li>Increment failed operations</li>
+     * </ul>
+     *
+     * @param photos Collection of photo records
+     */
     void migratePhotos(Collection<Photo> photos);
 
+    /**
+     * Get success count.
+     *
+     * @return Number of successful migration ops
+     */
     long getSuccessCount();
 
+    /**
+     * Get failure count.
+     *
+     * @return Number of failed migration ops
+     */
     long getFailureCount();
 }

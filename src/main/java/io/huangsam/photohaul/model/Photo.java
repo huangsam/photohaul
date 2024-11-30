66a89ad44f73d6d746cff0fe408bbade41f05603
Samuel Huang <samhuang91@gmail.com>
2024-11-30 10:08:50 -0800

    Refine JavaDoc for Photo record

--- src/main/java/io/huangsam/photohaul/model/Photo.java
+++ src/main/java/io/huangsam/photohaul/model/Photo.java
@@ -13,9 +13,9 @@ import java.time.format.DateTimeFormatter;
 /**
  * Represents a photo with its metadata.
  *
- * <p> Note that not all photos will have this metadata. This metadata exists on RAW formats
- * from camera providers such as Nikon, Canon and Sony. It also exists on JPG/JPEG files which
- * were recently processed from Adobe Lightroom.
+ * <p> Note that this metadata is not guaranteed for all photos. This metadata exists on
+ * RAW formats from providers such as Nikon, Canon and Sony. It also exists on JPG/JPEG
+ * files which were recently processed from Adobe Lightroom.
  */
 public record Photo(
         Path path,

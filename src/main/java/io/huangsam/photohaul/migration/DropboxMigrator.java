379b24b0e369a3dc57e7cbbc3a68c52adb93d55d
Samuel Huang <samhuang91@gmail.com>
2024-12-05 04:54:19 -0800

    Create simple Dropbox constructor

--- src/main/java/io/huangsam/photohaul/migration/DropboxMigrator.java
+++ src/main/java/io/huangsam/photohaul/migration/DropboxMigrator.java
@@ -1,10 +1,27 @@
 package io.huangsam.photohaul.migration;
 
+import com.dropbox.core.v2.DbxClientV2;
 import io.huangsam.photohaul.model.Photo;
+import org.slf4j.Logger;
 
 import java.util.Collection;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class DropboxMigrator implements Migrator {
+    private static final Logger LOG = getLogger(DropboxMigrator.class);
+
+    private final DbxClientV2 dropboxClient;
+    private final PhotoResolver photoResolver;
+
+    private long successCount = 0L;
+    private long failureCount = 0L;
+
+    public DropboxMigrator(DbxClientV2 dropboxClient, PhotoResolver photoResolver) {
+        this.dropboxClient = dropboxClient;
+        this.photoResolver = photoResolver;
+    }
+
     @Override
     public void migratePhotos(Collection<Photo> photos) {
 

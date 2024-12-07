f6d94ecbf3722b2d10faa0a9b06c0e46879db52d
Samuel Huang <samhuang91@gmail.com>
2024-12-07 13:57:56 -0800

    Refine existing factory tests

--- src/test/java/io/huangsam/photohaul/migration/TestMigratorFactory.java
+++ src/test/java/io/huangsam/photohaul/migration/TestMigratorFactory.java
@@ -5,19 +5,23 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 public class TestMigratorFactory {
     private static final MigratorFactory FACTORY = new MigratorFactory();
     private static final PhotoResolver RESOLVER = new PhotoResolver(List.of());
 
     @Test
-    void testMakePathMigrator() throws MigratorException {
+    void testMakePathMigratorClass() throws MigratorException {
         Settings settings = new Settings("path-example.properties");
-        FACTORY.make(MigratorMode.PATH, settings, RESOLVER);
+        Migrator migrator = FACTORY.make(MigratorMode.PATH, settings, RESOLVER);
+        assertSame(PathMigrator.class, migrator.getClass());
     }
 
     @Test
-    void testMakeDropboxMigrator() throws MigratorException {
+    void testMakeDropboxMigratorClass() throws MigratorException {
         Settings settings = new Settings("dbx-example.properties");
-        FACTORY.make(MigratorMode.DROPBOX, settings, RESOLVER);
+        Migrator migrator = FACTORY.make(MigratorMode.DROPBOX, settings, RESOLVER);
+        assertSame(DropboxMigrator.class, migrator.getClass());
     }
 }

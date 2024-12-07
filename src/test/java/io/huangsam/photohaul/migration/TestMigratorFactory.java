0317ce751bf8e77f51c34bee2f100c43e7eeb046
Samuel Huang <samhuang91@gmail.com>
2024-12-07 14:00:51 -0800

    Add factory test for failure

--- src/test/java/io/huangsam/photohaul/migration/TestMigratorFactory.java
+++ src/test/java/io/huangsam/photohaul/migration/TestMigratorFactory.java
@@ -6,22 +6,29 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestMigratorFactory {
     private static final MigratorFactory FACTORY = new MigratorFactory();
     private static final PhotoResolver RESOLVER = new PhotoResolver(List.of());
 
     @Test
-    void testMakePathMigratorClass() throws MigratorException {
+    void testMakePathMigratorSuccess() throws MigratorException {
         Settings settings = new Settings("path-example.properties");
         Migrator migrator = FACTORY.make(MigratorMode.PATH, settings, RESOLVER);
         assertSame(PathMigrator.class, migrator.getClass());
     }
 
     @Test
-    void testMakeDropboxMigratorClass() throws MigratorException {
+    void testMakeDropboxMigratorSuccess() throws MigratorException {
         Settings settings = new Settings("dbx-example.properties");
         Migrator migrator = FACTORY.make(MigratorMode.DROPBOX, settings, RESOLVER);
         assertSame(DropboxMigrator.class, migrator.getClass());
+    }
+
+    @Test
+    void testMakeGoogleDriveMigratorFailure() {
+        Settings settings = new Settings("drive-example.properties");
+        assertThrows(MigratorException.class, () -> FACTORY.make(MigratorMode.GOOGLE_DRIVE, settings, RESOLVER));
     }
 }

9d15c1625c47ce8bd1d825acbd175354bf077d33
Samuel Huang <samhuang91@gmail.com>
2024-12-07 16:44:36 -0800

    Update MigratorMode.java

--- src/main/java/io/huangsam/photohaul/migration/MigratorMode.java
+++ src/main/java/io/huangsam/photohaul/migration/MigratorMode.java
@@ -1,12 +1,12 @@
 package io.huangsam.photohaul.migration;
 
 public enum MigratorMode {
-    /** Migrates photos to a local directory path. */
+    /** Migrate photos to a local directory path. */
     PATH,
 
-    /** Migrates photos to a Dropbox account. */
+    /** Migrate photos to a Dropbox account. */
     DROPBOX,
 
-    /** Migrates photos to a Google Drive account. */
+    /** Migrate photos to a Google Drive account. */
     GOOGLE_DRIVE
 }

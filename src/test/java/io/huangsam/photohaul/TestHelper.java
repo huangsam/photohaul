ced232f4d6afdb4b27c427f16a1292036569f2ba
Samuel Huang <samhuang91@gmail.com>
2024-12-08 18:57:46 -0800

    Add NotNull to TestHelper

--- src/test/java/io/huangsam/photohaul/TestHelper.java
+++ src/test/java/io/huangsam/photohaul/TestHelper.java
@@ -3,6 +3,7 @@ package io.huangsam.photohaul;
 import io.huangsam.photohaul.migration.PathMigrator;
 import io.huangsam.photohaul.migration.PhotoResolver;
 import io.huangsam.photohaul.traversal.PhotoPathCollector;
+import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
 import java.util.List;
@@ -10,15 +11,18 @@ import java.util.List;
 public class TestHelper {
     private static final Path TEST_RESOURCES = Path.of("src/test/resources");
 
+    @NotNull
     public static Path getStaticResources() {
         return TEST_RESOURCES.resolve("static");
     }
 
+    @NotNull
     public static Path getTempResources() {
         return TEST_RESOURCES.resolve("temp");
     }
 
-    public static PhotoPathCollector getPathCollector(Path path, List<String> names) {
+    @NotNull
+    public static PhotoPathCollector getPathCollector(Path path, @NotNull List<String> names) {
         PhotoPathCollector pathCollector = new PhotoPathCollector();
         for (String name : names) {
             pathCollector.addPhoto(path.resolve(name));
@@ -26,6 +30,7 @@ public class TestHelper {
         return pathCollector;
     }
 
+    @NotNull
     public static PathMigrator getPathMover(Path destination) {
         return new PathMigrator(destination, PathMigrator.Action.DRY_RUN, new PhotoResolver(List.of()));
     }

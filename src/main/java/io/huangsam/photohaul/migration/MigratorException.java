5781d841feaab7fe661cb37d50d300e43232a2b5
Samuel Huang <samhuang91@gmail.com>
2024-12-08 17:58:01 -0800

    Add JavaDoc for exception class

--- src/main/java/io/huangsam/photohaul/migration/MigratorException.java
+++ src/main/java/io/huangsam/photohaul/migration/MigratorException.java
@@ -1,5 +1,12 @@
 package io.huangsam.photohaul.migration;
 
+/**
+ * This resembles an issue with methods called in {@link MigratorFactory}
+ * and {@link Migrator}.
+ *
+ * <p> To provide more context on the issue origin, we provide {@code mode} as
+ * another constructor field.
+ */
 public class MigratorException extends Exception {
     private final MigratorMode mode;
 

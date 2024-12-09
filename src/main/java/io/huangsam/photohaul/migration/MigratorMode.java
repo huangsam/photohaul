8b744259533c5a894055b30c629df479a4b5a7ec
Samuel Huang <samhuang91@gmail.com>
2024-12-08 17:43:49 -0800

    Add more docs for MigratorMode

--- src/main/java/io/huangsam/photohaul/migration/MigratorMode.java
+++ src/main/java/io/huangsam/photohaul/migration/MigratorMode.java
@@ -1,5 +1,13 @@
 package io.huangsam.photohaul.migration;
 
+/**
+ * This mode determines whether photos are migrated between local folders or
+ * migrated to a cloud service.
+ *
+ * <p> Please add the necessary fields to {@code config.properties} based on
+ * the selected mode. Check sample configurations in {@code src/main/resources}
+ * for reference.
+ */
 public enum MigratorMode {
     /** Migrate photos to a local directory path. */
     PATH,
